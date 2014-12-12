820d202ef927dc3b23a2b69ffa06b3e19c8484d0
Yan Wang <grapeot@gmail.com>
2014-12-12 17:29:14 -0500

    update the merger to make it more useful.

--- src/merger.cpp
+++ src/merger.cpp
@@ -6,28 +6,44 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    string ifn1 = "forest1.dat", ifn2 = "forest2.dat", outfn("forest.dat");
-    cout << "Usage: merger <in1 = forest1.dat> <in2 = forest2.dat> <out = forest.dat>" << endl;
-    if (argc == 4) {
-        ifn1 = string(argv[1]);
-        ifn2 = string(argv[2]);
-        outfn = string(argv[3]);
-    }
-    else {
+    if (argc < 4) {
         cout << "Wrong number of parameters." << endl;
+        cout << "Usage: merger <out = forest.dat> <list of forest files>" << endl;
+        exit(-1);
     }
 
+    // first random forest with data structure
+    string ifn1 = argv[2];
     ifstream ifp1(ifn1, ios::binary);
-    ifstream ifp2(ifn2, ios::binary);
-    if (!ifp1 || !ifp2) {
+    if (!ifp1) {
         cerr << "Cannot open the input files!" << endl;
         return -1;
     }
+    RF<labelNum, dim> rf;
+    int count = 0, dti = 0;
+    ifp1.read((char *)&count, sizeof(int));
+    rf.dts.reserve(count * (argc - 2));
+    cout << "Merging random forest " << 1 << " from " << ifn1 << ", with " << count << " trees..." << endl;
+    for(; dti < count; dti++)
+        rf.dts.push_back(DT<labelNum, dim>::ParseFromStream(ifp1));
 
-    auto rf = RF<labelNum, dim>::MergeRFs(ifp1, ifp2);
-    ifp1.close();
-    ifp2.close();
+    // remaining ones
+    ifstream ifp2;
+    for (int i = 3; i < argc; i++) {
+        ifp2.open(argv[i], ios::binary);
+        if (!ifp2) {
+            cerr << "Cannot open the input files!" << endl;
+            return -1;
+        }
+        ifp2.read((char *)&count, sizeof(int));
+        cout << "Merging random forest " << i - 1 << " from " << argv[i] << ", with " << count << " trees..." << endl;
+        for(int j = 0; j < count; j++)
+            rf.dts.push_back(DT<labelNum, dim>::ParseFromStream(ifp2));
+        ifp2.close();
+        ifp2.clear();
+    }
 
+    string outfn = argv[1];
     ofstream ofp(outfn, ios::binary);
     if (!ofp) {
         cerr << "Cannot open the output file!" << endl;
@@ -35,7 +51,6 @@ int main(int argc, char **argv) {
     }
     cout << "The merged RF has " << rf.dts.size() << " trees." << endl;
     rf.Serialize(ofp);
-
     ofp.close();
 
     return 0;
